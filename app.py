b9e407e73ed61110f7aea79deb613df8ef5a6af6
Rajasimman S <srajasimman@gmail.com>
2024-12-17 21:56:43 +0530

    :art: code format

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
